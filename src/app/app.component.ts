6dc4f31caf40d387c02e5593803a94e15927b671
illurito <danielalberto3004@gmail.com>
2024-06-28 17:20:56 -0600

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'libros';
+  constructor(private router: Router) {}
+
+  menu() {
+    this.router.navigate(['/listado-libros']);
+  }
+
+  agregar() {
+    this.router.navigate(['/agregar-libros']);
+  }
+
+  sounds: string[] = [
+    'assets/sound/16.wav',
+    'assets/sound/17.wav',
+    'assets/sound/26.wav',
+    'assets/sound/30.wav',
+    'assets/sound/35.wav',
+    'assets/sound/38.wav',
+    'assets/sound/45.wav',
+    'assets/sound/62.wav'
+  ];
+
+  reproducirNota() {
+    const randomIndex = Math.floor(Math.random() * this.sounds.length);
+    const audio = new Audio(this.sounds[randomIndex]);
+    audio.playbackRate = 0.2;
+    audio.volume = 0.5; 
+    audio.play();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AgregarLibroComponent } from './pages/agregar-libro/agregar-libro.component';
+import { ListadoLibrosComponent } from './pages/listado-libros/listado-libros.component';
+import { EditarLibroComponent } from './pages/editar-libro/editar-libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarLibroComponent,
+    ListadoLibrosComponent,
+    EditarLibroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
